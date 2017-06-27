8c81eec2bac73e63a106b1ef336f0e9d825ad55b
zanjs <root@zanjs.com>
2017-06-27 22:45:27 +0800

    Those who turn back never reach the summit. :horse:

---
+++ database/migrations/2017_06_27_133213_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('group_id',50)->notNull()->comment('所属团编号');
+            $table->string('name',100)->default(null)->comment('商品名称');
+            $table->decimal('price',12,2)->default(0.00)->comment('商品价格');
+            $table->integer('quantity')->default(null)->comment('商品数量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2017_06_27_142718_create_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description')->notNull()->comment('拼团描述');
+            $table->string('type')->notNull()->comment('拼团类型');
+            $table->string('currency_id',50)->notNull()->comment('货币类型ID');
+            $table->string('head_id',50)->notNull()->comment('团长ID');
+            $table->integer('open')->default(1)->comment('接龙状态 1 为接龙中 2 已截止');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
