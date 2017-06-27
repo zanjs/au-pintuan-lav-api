35066132d26aa29b2b403f33fc24b1b03cd9b7bf
zanjs <root@zanjs.com>
2017-06-27 09:15:56 +0800

    Winners do what losers don't want to do :cloud:

--- app/Http/Controllers/WeUserController.php
+++ app/Http/Controllers/WeUserController.php
@@ -6,11 +6,37 @@ use Illuminate\Http\Request;
 use App\WeUser;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Iwanli\Wxxcx\Wxxcx;
 use Auth;
 
 class WeUserController extends Controller
 {
     protected $guard = 'api';
+
+    protected $wxxcx;
+
+    function __construct(Wxxcx $wxxcx)
+    {
+        $this->wxxcx = $wxxcx;
+    }
+
+    /**
+     * 小程序登录获取用户信息
+     */
+    public function getWxUserInfo()
+    {
+        //code 在小程序端使用 wx.login 获取
+        $code = request('code', '');
+        //encryptedData 和 iv 在小程序端使用 wx.getUserInfo 获取
+        $encryptedData = request('encryptedData', '');
+        $iv = request('iv', '');
+
+        //根据 code 获取用户 session_key 等信息, 返回用户openid 和 session_key
+        $userInfo = $this->wxxcx->getLoginInfo($code);
+
+        //获取解密后的用户信息
+        return $this->wxxcx->getUserInfo($encryptedData, $iv);
+    }
 
     /**
      * 微信用户自动登陆
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('/login','WeUserController@login');
+Route::post('/wx-info','WeUserController@getWxUserInfo');
