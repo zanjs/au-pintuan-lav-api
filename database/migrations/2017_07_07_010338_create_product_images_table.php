8fd7d391c3637a21eae43e4aee419aa32dc4ea18
zanjs <root@zanjs.com>
2017-07-07 09:08:28 +0800

    git log 查看 commit日志 :mouse2:

---
+++ database/migrations/2017_07_07_010338_create_product_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_id',50)->notNull()->comment('所属产品ID');
+            $table->string('path')->notNull()->comment('存放路径');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
